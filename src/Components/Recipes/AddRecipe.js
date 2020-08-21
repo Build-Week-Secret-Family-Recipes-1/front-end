087d8611a349623e3b326a44d875a007079ed626
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-08-20 22:15:23 -0400

    added form to AddRecipe

--- src/Components/Recipes/AddRecipe.js
+++ src/Components/Recipes/AddRecipe.js
@@ -1,10 +1,56 @@
 import React from "react";
+import styled from "styled-components";
+
+const [recipe, setRecipe] = useState({
+    title: "",
+    source: "",
+    ingredients: "",
+    instructions: "",
+    category: ""
+})
+
+const Category = ["Breakfast", "Lunch", "Dinner"]
 
 const AddRecipe = () => {
 
+    handleChange = () => {
+
+    }
+
+    submit = () => {
+        
+    }
+
     return (
         <div>
+            <div>
+            <form>
+                <label htmlFor="title">
+                    Title:
+                    <input type="text" id="title" name="title" value={recipe.name} onChange={handleChange} />
+                </label>
+                <label htmlFor="source">
+                    Source: 
+                    <input type="text" id="source" name="source" value={recipe.source} />
+                </label>
+                <label htmlFor="ingredients">
+                    Ingredients:
+                    <input type="text" id="ingredients" name="ingredients" value={recipe.ingredients} />
+                </label>
+                <label htmlFor="instructions">
+                    <textarea type="text" id="instructions" name="instructions" value={recipe.instructions} />
+                </label>
+                <label htmlFor="category">
+                    <select>
+                        <option>--Please Select A Category--</option>
+                        {Category.map((meal) => {
+                            return <option value={meal} key={meal}>{meal}</option>
+                        })}
+                    </select>
 
+                </label>
+            </form>
+        </div>
         </div>
     )
 }
