b31377837b4abbf5ef026325f050501111052329
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-08-21 22:18:36 -0400

    Added form validation

--- src/Components/Recipes/AddRecipe.js
+++ src/Components/Recipes/AddRecipe.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
+import * as yup from "yup";
 
 
 const Category = ["Breakfast", "Lunch", "Dinner"]
@@ -14,11 +15,47 @@ const AddRecipe = () => {
         category: ""
     })
     const handleChange = (e) => {
+        e.persist();
         const NewRecipe = {
             ...recipe,
             [e.target.name]: e.target.value
         }
+        validateChange(e);
         setRecipe(NewRecipe);
+    }
+
+    const recipeSchema = yup.object().shape({
+        title: yup.string().required("Please enter a title."),
+        source: yup.string().required("Please enter a source."),
+        ingredients: yup.string().required("Please enter ingredients."),
+        instructions: yup.string.required("Please enter the instructions."),
+        category: yup.string().required("A category is required."),
+    })
+
+    const [errors, setErrors] = useState({
+        title: "",
+        source: "",
+        ingredients: "",
+        instructions: "",
+        category: ""
+    })
+
+    const validateChange = (e) => {
+        yup
+        .reach(recipeSchema, e.target.name)
+        .validate(e.target.value)
+        .then((valid) => {
+            setErrors({
+                ...errors,
+                [e.target.errors]: ""
+            })
+        })
+        .catch((err) => {
+            setErrors({
+                ...errors,
+                [e.target.name]: err.errors[0]
+            })
+        })
     }
 
     const submit = (e) => {
@@ -37,22 +74,26 @@ const AddRecipe = () => {
     return (
         <div>
             <div>
-            <form>
+            <form onSubmit={submit}>
                 <label htmlFor="title">
                     Title:
                     <input type="text" id="title" name="title" value={recipe.name} onChange={handleChange} />
+                    {errors.title.length > 0 ? <p classname="errors">{errors.title}</p> : null}
                 </label>
                 <label htmlFor="source">
                     Source: 
                     <input type="text" id="source" name="source" value={recipe.source} />
+                    {errors.source.length > 0 ? <p>{errors.source}</p> : null}
                 </label>
                 <label htmlFor="ingredients">
                     Ingredients:
                     <input type="text" id="ingredients" name="ingredients" value={recipe.ingredients} />
+                    {errors.ingredients.length > 0 ? <p>{errors.ingredients}</p> : null}
                 </label>
                 <label htmlFor="instructions">
                     Instructions: 
                     <textarea type="text" id="instructions" name="instructions" value={recipe.instructions} />
+                    {errors.instructions.length > 0 ? <p>{errors.instructions}</p> : null}
                 </label>
                 <label htmlFor="category">
                     <select>
@@ -60,9 +101,11 @@ const AddRecipe = () => {
                         {Category.map((meal) => {
                             return <option value={meal} key={meal}>{meal}</option>
                         })}
+                        {errors.category.length > 0 ? <p>{errors.category}</p> : null}
                     </select>
 
                 </label>
+                <button type="submit">Add Recipe</button>
             </form>
         </div>
         </div>
